b54386f79cc37f31695df56284bd5444483b1c9c
Shaun Koo <shaunktw@gmail.com>
2014-08-03 12:33:24 -0400

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Textrecon::Application.config.secret_key_base = 'c20dcce4f88bf899922db9f862eee905ec23bbfde37df751ea9e21262b3f01a7f7a535c5438d82b93c36eae176ac84f4e9b31da45cb59d230fad4536b6d39865'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Textrecon::Application.config.session_store :cookie_store, key: '_textrecon_session'
