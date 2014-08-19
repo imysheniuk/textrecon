c6b341a912da4de989948a40d87c330c012d4063
Shaun Koo <shaunktw@gmail.com>
2014-08-19 07:52:12 -0400

    Added redis for production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
-REDIS = Redis.new
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  REDIS = Redis.new
+end
